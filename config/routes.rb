020e40993d2c2920de92d6ada0472a4649adcf12
cle61 <prodhomme.clement@hotmail.fr>
2015-09-14 14:12:43 +0200

    ajout du controller news

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  resources :news, only: [:index, :show]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
