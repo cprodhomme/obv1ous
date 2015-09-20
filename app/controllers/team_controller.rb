e6f6c6d6a454810a8cf7fb81379d1d7946a8f598
cle61 <prodhomme.clement@hotmail.fr>
2015-09-20 14:27:41 +0200

    Modif controller team & welcome + view connexion & index

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,10 +1,10 @@
 class TeamController < ApplicationController
 
     def index
-
+    	@team = Team.order('id desc')
     end
 
     def show
-
+    	@member = Team.find(params[:id])
     end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
 
     def index
-
+    	@news = News.order('id desc')
     end
 end
