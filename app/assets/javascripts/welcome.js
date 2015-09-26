0314b6650bbe34511706fe43d9b127d2c29aa828
Quentin Boussard <quentin.bouss@gmail.com>
2015-09-26 22:15:16 +0200

    Header fun

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,3 +1,19 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+var k = [79, 66, 86, 49],
+n = 0;
+$(document).keydown(function (e) {
+    if (e.keyCode === k[n++]) {
+        if (n === k.length) {
+            $('.awpheader').animate({
+                left: "0",
+            }, 3000, function() {
+
+              });
+            n = 0;
+            return false;
+        }
+    }
+    else {
+        n = 0;
+    }
+
+});
