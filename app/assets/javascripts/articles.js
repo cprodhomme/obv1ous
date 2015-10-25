bcf68c945dc8c94f79799a560dcf76fa8894d15c
Quentin Boussard <quentin.bouss@gmail.com>
2015-10-25 19:01:25 +0100

    WIP show user

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,24 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-$( document ).ready(function() {
-    var shoot = 0;
-
-    $('.logo_obv').click(function(){
-
-        if (shoot < 8) {
-            shoot = shoot +1;
-
-            $('.awpheader').animate({
-                left: "0",
-            }, 1500, function() {
-                $(".logo-img").attr("src","assets/logo-shoot-"+shoot+".png");
-            }).delay( 400 );
-
-            $('.awpheader').animate({
-                left: "-200px",
-            },1500);
-        }
-
-    });
-});
