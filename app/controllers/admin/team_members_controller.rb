05eaa96665165fbb2285f4f6fdf0f644a5a8397a
cle61 <prodhomme.clement@hotmail.fr>
2015-11-19 00:08:50 +0100

    WIP admin/team_members - add variables

--- app/controllers/admin/team_members_controller.rb
+++ app/controllers/admin/team_members_controller.rb
@@ -1,4 +1,85 @@
 class Admin::TeamMembersController < Admin::DashboardController
+	#http://ddragon.leagueoflegends.com/cdn/5.22.3/img/champion/TahmKench.png
+
+	@weapons = ['M4A1-s',
+				'M4A4',
+				'AWP',
+				'P90',
+				'Five-seveN',
+				'Deagle',
+				'AK-47',
+				'SSG 08']
+
+	@champions = ['Jinx',
+				'Tristana',
+				'Lucian',
+				'Thresh',
+				'Braum',
+				'Orianna',
+				'Anivia',
+				'Tahm Kench']
+
+	@rank_cs = ['',
+				'Silver I',
+				'Silver II',
+				'Silver III',
+				'Silver IV',
+				'Silver Elite',
+				'Silver Elite Master',
+				'Gold Nova I',
+				'Gold Nova II',
+				'Gold Nova III',
+				'Gold Nova Master',
+				'Master Guardian I',
+				'Master Guardian II',
+				'Master Guardian Elite',
+				'Distinguished Master Guardian',
+				'Legendary Eagle',
+				'Legendary Eagle Master',
+				'Supreme Master First Class',
+				'The Global Elite',
+				]
+
+	@rank_lol = ['',
+				'Bronze 5',
+				'Bronze 4',
+				'Bronze 3', 
+				'Bronze 2',
+				'Bronze 1', 
+				'Silver 5',
+				'Silver 4',
+				'Silver 3',
+				'Silver 2',
+				'Silver 1',
+				'Gold 5',
+				'Gold 4',
+				'Gold 3',
+				'Gold 2',
+				'Gold 1',
+				'Platine 5',
+				'Platine 4',
+				'Platine 3',
+				'Platine 2',
+				'Platine 1',
+				'Diamond 5',
+				'Diamond 4',
+				'Diamond 3',
+				'Diamond 2',
+				'Diamond 1',
+				'Master',
+				'Challenger']
+
+	@role_lol = ['',
+				 'Top',
+				 'Jungle',
+				 'Mid',
+				 'ADC',
+				 'Support']
+
+	@role_lol = ['',
+				 'Open Frag',
+				 'AWP',
+				 'Support']
 
 	def index
 		@members = TeamMember.order('id desc')
