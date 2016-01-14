95793817214077385640377073a38a07df87a49e
Quentin Boussard <quentin.bouss@gmail.com>
2016-01-14 13:55:30 +0100

    WIP steam api connection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,13 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
+	after_action :set_access_control_headers
+
+	def set_access_control_headers
+		headers['Access-Control-Allow-Origin'] = "*"
+		headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
+	end
+
 	protected
 
 	def configure_permitted_parameters
