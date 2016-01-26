e481a4b1e893f1c50056298cb62b51fe7d9f62b8
Quentin Boussard <quentin.bouss@gmail.com>
2016-01-26 10:36:27 +0100

    edit seed and team for no id steam

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -14,7 +14,7 @@ class TeamController < ApplicationController
         @weapons = TeamMembersWeapon.where(team_member_id: params[:id]).all
         @champions = TeamMembersChampion.where(team_member_id: params[:id]).all
 
-
+        if(@member.id_steam != '')
           	source = 'http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=400CE5B4A6702A1A9EFD2822BC461A9D&steamid='+@member.id_steam
 
           	uri = URI(source)
@@ -47,6 +47,6 @@ class TeamController < ApplicationController
                             @ratioKD = (@total_kill / @total_death.to_f).round(2)
                             @killByRound = (@total_kill / @total_rounds_played.to_f).round(1)
                 end
-
+            end
     end
 end
